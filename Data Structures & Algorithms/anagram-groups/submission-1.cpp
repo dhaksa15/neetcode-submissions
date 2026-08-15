6e8677625914ba20f763d24d544421c29cadeb39
DHAKSA M <dhaksa.nm@gmail.com>
2026-08-15 17:08:13 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      unordered_map<string,vector<string>>mp;
+      for(string s:strs){
+        string k=s;
+        sort(k.begin(),k.end());
+        mp[k].push_back(s);
+
+      }
+      vector<vector<string>>res;
+      for(auto it:mp){
+        res.push_back(it.second);
+      }
+      return res;
+    }
+};
